7e8b19dad8d34f2567f8eda907b99789186638a2
iAmGroute <pgonis@gmail.com>
2019-05-05 01:17:21 +0300

    many changes

--- NPTpy/Servers/Relay.py
+++ NPTpy/Servers/Relay.py
@@ -5,35 +5,70 @@
 
 import logging
 import socket
+from recordclass import recordclass
 
 from Common.Connector import Connector
 
-log   = logging.getLogger(__name__)
-logPT = logging.getLogger(__name__ + ':PT')
-# logPU = logging.getLogger(__name__ + ':PU')
+log   = logging.getLogger(__name__ + '   ')
+logST = logging.getLogger(__name__ + ':ST')
+# logSU = logging.getLogger(__name__ + ':SU')
 
-class ConnSocket(socket.socket):
-    token = b''
-    other = None
+class RelayConnSocket:
+    def __init__(self, baseSocket):
+        self.baseSocket = baseSocket
+        self.token = b''
+        self.other = None
+
+    def recv(self, size):
+        return self.baseSocket.recv(size)
+
+    def sendall(self, data):
+        return self.baseSocket.sendall(data)
 
     def tryClose(self):
         try:
-            self.close()
+            self.baseSocket.close()
         except socket.error:
             return False
         return True
 
-MapRecord = namedtuple('MapRecord', ['indexA', 'indexB'])
+MapRecord = recordclass('MapRecord', ['indexA', 'indexB'])
 
-class Server:
+class Relay:
 
     def __init__(self, port, address='0.0.0.0'):
-        self.con   = Connector(log,   socket.SOCK_STREAM, None, port, address)
-        self.conPT = Connector(logPT, socket.SOCK_STREAM, None, port, address)
+        self.con   = Connector(log,   socket.SOCK_STREAM, None, port, address) # for portals/clients
+        self.conST = Connector(logST, socket.SOCK_STREAM, None, 40401, '0.0.0.0') # for server
         self.con.listen()
-        self.conPT.listen()
+        self.conST.listen()
         self.connSockets = [] # TODO: convert to pool allocator
         self.tokenMap    = {} # Dictionary (hash table)
+
+    def taskManage(self):
+        conn, addr = self.conST.accept()
+        # while conn:
+        with conn:
+            try:
+                data = conn.recv(20)
+            except socket.error:
+                pass
+            else:
+                if len(data) == 20:
+                    verb     = data[0:4]
+                    callerID = data[4:8]
+                    otherID  = data[8:12]
+                    token    = data[12:20]
+                    log.info('    new command:')
+                    log.info('    verb:{0} callerID:{1} otherID:{2} token{3}'.format(verb, callerID.hex(), otherID.hex(), token.hex()))
+
+                    # We don't store the IDs, although we should, for validation/security check
+                    if verb == b'ADD.':
+                        self.removeByToken(token)
+                        self.tokenMap[token] = MapRecord(-1, -1)
+                    elif verb == b'DEL.':
+                        self.removeByToken(token)
+
+                    conn.sendall(b'OK')
 
     def task(self):
         conn, addr = self.con.accept()
@@ -61,17 +96,17 @@ class Server:
                 if rec.indexA == -1:
                     # First one to connect
 
-                    newSocket       = ConnSocket(conn)
+                    newSocket       = RelayConnSocket(conn)
                     newSocket.token = token
                     rec.indexA = len(self.connSockets)
                     self.connSockets.append(newSocket)
 
-                    log.info('    indexA: {3d}'.format(rec.indexA))
+                    log.info('    indexA: {0:3d}'.format(rec.indexA))
 
                 elif rec.indexB == -1:
                     # Second one to connect
 
-                    newSocket       = ConnSocket(conn)
+                    newSocket       = RelayConnSocket(conn)
                     newSocket.token = token
                     newSocket.other = self.connSockets[rec.indexA]
                     rec.indexB = len(self.connSockets)
@@ -80,7 +115,7 @@ class Server:
                     # we can now forward between the 2
                     self.connSockets[rec.indexA].other = newSocket
 
-                    log.info('    indexA: {3d}, indexB: {3d}'.format(rec.indexA, rec.indexB))
+                    log.info('    indexA: {0:3d}, indexB: {1:3d}'.format(rec.indexA, rec.indexB))
 
                 else:
                     log.info('    REUSE')
@@ -116,11 +151,21 @@ class Server:
             self.removeIndex(rec.indexB)
             del self.tokenMap[conn.token]
 
-    def proccess(self, conn):
+    def removeByToken(self, token):
+        try:
+            rec = self.tokenMap[token]
+        except KeyError:
+            pass
+        else:
+            self.closeByIndex(rec.indexA)
+            self.closeByIndex(rec.indexB)
+            del self.tokenMap[token]
+
+    def process(self, conn):
         data = conn.recv(2048)
         if len(data) < 1:
             # Connection closed (?), remove both
             self.removeConn(conn)
 
         if conn.other:
-            conn.other.sendall(reply)
+            conn.other.sendall(data)
--- NPTpy/Servers/Server.py
+++ NPTpy/Servers/Server.py
@@ -2,43 +2,79 @@
 
 import logging
 import socket
+from recordclass import recordclass
 
 from Common.Connector import Connector
 
-log = logging.getLogger(__name__)
+log   = logging.getLogger(__name__ + '   ')
+logRT = logging.getLogger(__name__ + ':RT')
+# logRU = logging.getLogger(__name__ + ':RU')
 
-class ConnSocket(socket.socket):
-    portalIndex = -1
+class ServerConnSocket:
+    def __init__(self, baseSocket):
+        self.baseSocket = baseSocket
+        self.portalIndex = -1
 
-class Methods:
-    # A method returns <(reply, close)>,
-    # where <reply> is:
-    #   - a bytearray with the data to send as a reply
-    #   - a bytearray which is empty (b'') and then no reply is sent
-    # and <close> is
-    #   - True, if the connection should be closed right after sending the reply
-    #   - False, if it should be kept open
-    @staticmethod
-    def InvalidMethod(message):
-        return b'\x00', True
-    @staticmethod
-    def Connect(message):
-        return b'', False
+    def recv(self, size):
+        return self.baseSocket.recv(size)
 
-methodTable = [
-    Methods.InvalidMethod,
-    Methods.Connect
-]
+    def sendall(self, data):
+        return self.baseSocket.sendall(reply)
 
-PortalRecord = namedtuple('PortalRecord', ['portalID', 'addr', 'conn'])
+PortalRecord = recordclass('PortalRecord', ['portalID', 'addr', 'conn'])
+
+RelayManageAddr = '127.0.0.1'
+RelayManagePort = 40401
 
 class Server:
 
     def __init__(self, port, address='0.0.0.0'):
-        self.con = Connector(log, socket.SOCK_STREAM, None, port, address)
+        self.con   = Connector(log, socket.SOCK_STREAM, None, port, address)
+        self.conRT = Connector(log, socket.SOCK_STREAM, None, 40402, '0.0.0.0')
         self.con.listen()
+        self.conRT.connect((RelayManageAddr, RelayManagePort))
         self.portalTable   = [] # TODO: convert to pool allocator
         self.portalIndexer = {} # Dictionary (hash table)
+
+    class Methods:
+        # A method returns <(reply, close)>,
+        # where <reply> is:
+        #   - a bytearray with the data to send as a reply
+        #   - a bytearray which is empty (b'') and then no reply is sent
+        # and <close> is
+        #   - True, if the connection should be closed right after sending the reply
+        #   - False, if it should be kept open
+
+        # Catch-all for all method codes which are not valid
+        def InvalidMethod(self, record, message):
+            return b'Invalid Mathod', True
+
+        # Request to connect to portal
+        def ConnectToPortal(self, record, message):
+            otherID = message[4:8]
+            log.info('    wants to connect to: x{0}'.format(otherID.hex()))
+            try:
+                otherIndex = self.portalIndexer[otherID]
+            except KeyError:
+                return b'Bad ID', True
+            else:
+                log.info('    found')
+                self.notifyRelay(record.portalID, otherID, b'01234567') # TODO: generate with system urandom (crypto)
+                # TODO: add the following to a task queue and wait for positive reply from relay
+                # before notifying the portal
+                otherRecord = self.portalTable[otherIndex]
+                self.notifyPortal(otherRecord, record)
+                return b'realy_ip:port', True # this should be False, until we get confirm from relay & portal
+
+    def notifyRelay(self, callerID, otherID, token):
+        msg = b'ADD.'   # 4B
+        msg += callerID # 4B
+        msg += otherID  # 4B
+        msg += token    # 8B
+        self.conRT.sendall(msg) # 20B
+
+    def notifyPortal(self, portalRecord, callerRecord):
+        portalRecord.conn.sendall(b'realy_ip:port') # TODO: also send caller's info from callerRecord
 
     def task(self):
         conn, addr = self.con.accept()
@@ -59,8 +95,9 @@ class Server:
             # TODO: authenticate
             conn.sendall(b'\x00')
 
-            record = PortalRecord(portalID, addr, ConnSocket(conn))
+            record = PortalRecord(portalID, addr, ServerConnSocket(conn))
 
+            # Add the record or update the existing one
             try:
                 portalIndex = self.portalIndexer[portalID]
             except KeyError:
@@ -85,17 +122,33 @@ class Server:
         del self.portalIndexer[portalID]
 
     def proccess(self, conn):
+        try:
+            record = self.portalTable[conn.portalIndex]
+        except KeyError:
+            # Connection is not registered, this shouldn't happen
+            log.error('    NOT IN TABLE')
+            self.removeConn(conn)
+            return
+
         data = conn.recv(64)
         if len(data) != 64:
             # Bad request, drop it
+            log.info('    bad request')
             self.removeConn(conn)
+            return
 
         methodID = int.from_bytes(data[0:4], 'little')
         if methodID >= len(methodTable):
             methodID = 0 # Invalid method
 
-        reply, close = methodTable[methodID](data)
+        reply, close = methodTable[methodID](self, record, data)
         if reply:
             conn.sendall(reply)
         if close:
             self.removeConn(conn)
+
+
+methodTable = [
+    Server.Methods.InvalidMethod,
+    Server.Methods.ConnectToPortal
+]
