0e7ca6d1df51d4ea137b4e8a9734cbac8c93452c
iAmGroute <pgonis@gmail.com>
2019-05-17 12:10:05 +0300

    Further changes in portal

--- NPTpy/portal.py
+++ NPTpy/portal.py
@@ -16,17 +16,28 @@ ServerPort = 4020
 
 
 class DeviceConn(Connector):
-    index = -1
     conRT = None
+    def task(self):
+        data = self.tryRecv(32768)
+        if len(data) < 1:
+            self.conRT.disconnect()
+            return
+        self.conRT.sendall(data)
 
 
 class RelayConnector(Connector):
+
+    class Modes(Enum):
+        Portal = 0
+        Client = 1
+
     class States(Enum):
         Disconnected = 0
-        WaitClient   = 1
-        Connecting   = 2
+        WaitReady    = 1
+        WaitDevice   = 2
         Ready        = 3
 
+    mode   = Modes.Portal
     state  = States.Disconnected
     parent = None
     index  = -1
@@ -34,9 +45,13 @@ class RelayConnector(Connector):
 
     def task(self):
         if   self.state == self.States.Disconnected: return
-        elif self.state == self.States.WaitClient:   self.waitReady()
-        elif self.state == self.States.WaitClient:   self.connectTo()
+        elif self.state == self.States.WaitReady:    self.waitReady()
+        elif self.state == self.States.WaitDevice:   self.waitDevice()
         elif self.state == self.States.Ready:        self.forward()
+
+    def disconnect(self):
+        self.state = self.States.Disconnected
+        self.tryClose()
 
     def connectRelay(self, token, relayPort, relayAddr):
         data = b''
@@ -44,7 +59,7 @@ class RelayConnector(Connector):
         data += b'0' * 56
         if self.tryConnect((relayAddr, relayPort), data):
             self.setKeepAlive()
-            self.state = self.States.WaitClient
+            self.state = self.States.WaitReady
             return True
         else:
             return False
@@ -52,47 +67,65 @@ class RelayConnector(Connector):
     def waitReady(self):
         data = self.tryRecv(8)
         if data == b'Ready !\n':
-            self.state = self.States.Ready
+            self.state = self.States.WaitDevice
+            if self.mode == self.Modes.Client:
+                self.listenDV()
         else:
-            self.state = self.States.Disconnected
-            self.tryClose()
+            self.disconnect()
+
+    def listenDV(self):
+        connSocket, addr = self.conn.accept()
+        connSocket.settimeout(0.2)
+        connSocket.setblocking(False)
+        # We need to upgrade connSocket from socket.socket to Connector
+        self.conn = connSocket
+
+    def waitDevice(self):
+        if   self.mode == self.Modes.Portal: self.connectTo()
+        elif self.mode == self.Modes.Client: self.acceptDV()
 
     def connectTo(self):
-        relayInfo = self.tryRecv(1024)
-        l = len(relayInfo)
-        if l < 11:
-            if l == 0: self.conST = None
-            return
-        token     = relayInfo[0:8]
-        relayPort = int.from_bytes(relayInfo[8:10], 'little')
-        relayAddr = str(relayInfo[10:], 'utf-8')
+        deviceInfo = self.tryRecv(1024)
+        l = len(deviceInfo)
+        if l > 2:
+            devicePort = int.from_bytes(deviceInfo[0:2], 'little')
+            deviceAddr = str(deviceInfo[2:], 'utf-8')
 
-        conRT = DeviceConn(logRT, socket.SOCK_STREAM, 2, self.port, self.address)
-        for i in range(3):
-            if conRT.connectRelay(token, relayPort, relayAddr):
-                conRT.index = len(self.conRTs)
-                conRT.parent = self
-                self.conRTs.append(conRT)
-                break
-            else:
-                conRT.tryClose()
+            self.conn = DeviceConn(logDV, socket.SOCK_STREAM, 2, self.parent.port, self.parent.address)
+            for i in range(3):
+                if self.conn.tryConnect((deviceAddr, devicePort)):
+                    self.conn.conRT = self
+                    self.state = self.States.Ready
+                    self.parent.conDVs[self.index] = self.conn
+                    return
+                else:
+                    self.conn.tryClose()
+            self.conn = None
+        self.disconnect()
+
+    def acceptDV(self):
+        self.conn.listen()
 
     def forward(self):
-        if self.conn:
-            data = self.tryRecv(32768)
-            if len(data) < 1:
-                self.state = self.States.Disconnected
-            self.sendall(data)
+        data = self.tryRecv(32768)
+        if len(data) < 1:
+            self.conn.tryClose()
+            self.disconnect()
+            return
+        try:
+            self.conn.sendall(data)
+        except ConnectionAbortedError:
+            self.disconnect()
 
 class Portal:
 
     def __init__(self, portalID, port=0, address='0.0.0.0'):
-        self.portalID  = portalID
-        self.port      = port
-        self.address   = address
-        self.conST     = None
-        self.conRTs    = []
-        self.conDVs    = []
+        self.portalID = portalID
+        self.port     = port
+        self.address  = address
+        self.conST    = None
+        self.conRTs   = []
+        self.conDVs   = []
 
     def main(self):
         if not self.conST:
@@ -103,7 +136,7 @@ class Portal:
             readable, writable, exceptional = select.select(socketList, [], [])
             for s in readable:
                 if   s is self.conST: self.task()
-                else:                 self.taskRT(s) # s is in self.conRTs or .conDVs
+                else:                 self.taskRTorDV(s) # s is in self.conRTs or .conDVs
 
     def connectKA(self):
         self.conST = Connector(logST, socket.SOCK_STREAM, 2, self.port, self.address)
@@ -129,14 +162,18 @@ class Portal:
                 conRT.parent = self
                 conRT.index = len(self.conRTs)
                 self.conRTs.append(conRT)
+                self.conDVs.append(None)
                 break
             else:
                 conRT.tryClose()
 
-    def taskRT(self, conRT):
-        conRT.task()
+    def taskRTorDV(self, con):
+        con.task()
+
+        conRT = con if isinstance(con, RelayConnector) else con.conRT
         if conRT.state == conRT.States.Disconnected:
             self.conRTs[conRT.index] = None
+            self.conDVs[conRT.index] = None
 
     # 'Client' mode
     # Notice that the behaviour of the server is symmetric
