649c6df4967ef07a39d00e104fe89d2e265e00a3
iAmGroute <pgonis@gmail.com>
2019-05-17 12:09:18 +0300

    Create testClient.py

---
+++ NPTpy/testClient.py
@@ -0,0 +1,22 @@
+python = 3
+python
+
+import time
+import logging
+logging.basicConfig(format='%(created).3f [%(name)s|%(levelname)s] %(message)s', level=logging.INFO)
+
+from Portal import Portal
+
+c = Portal(b'ABCF')
+
+c.main()
+
+c.connectToPortal(b'ABCE')
+
+c.main()
+c.main()
+
+data = (8080).to_bytes(2, 'little')
+data += b'192.168.11.11'
+
+c.conRTs[0].sendall(data)
