5a5cc3ce34101144422fcb396113d911353518f0
iAmGroute <pgonis@gmail.com>
2019-05-27 14:50:29 +0300

    SecureConnector: allow unencrypted communication until secure() is called

--- NPTpy/Common/SecureConnector.py
+++ NPTpy/Common/SecureConnector.py
@@ -8,27 +8,17 @@ from .Connector import *
 
 class SecureClientConnector(Connector):
 
-    def __init__(self, log, mySocket):
-        Connector.__init__(self, log, mySocket)
-        self.rawSocket  = self.socket
-        self.socket     = None
-
     def secure(self, serverHostname, caFilename=None, caDirpath=None, caData=None):
         self.sslContext = ssl.create_default_context(cafile=caFilename, capath=caDirpath, cadata=caData)
-        self.socket     = self.sslContext.wrap_socket(self.rawSocket, server_hostname=serverHostname)
+        self.socket     = self.sslContext.wrap_socket(self.socket, server_hostname=serverHostname)
 
 
 class SecureServerConnector(Connector):
-
-    def __init__(self, log, mySocket):
-        Connector.__init__(self, log, mySocket)
-        self.rawSocket = self.socket
-        self.socket    = None
 
     def secure(self, certFilename, keyFilename=None, keyPassword=None):
         self.sslContext          = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         self.sslContext.options |= ssl.OP_NO_TLSv1
         self.sslContext.options |= ssl.OP_NO_TLSv1_1
         self.sslContext.load_cert_chain(certFilename, keyFilename, keyPassword)
-        self.socket = self.sslContext.wrap_socket(self.rawSocket, server_side=True)
+        self.socket = self.sslContext.wrap_socket(self.socket, server_side=True)
 
