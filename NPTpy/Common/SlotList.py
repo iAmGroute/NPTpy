cc8d682bcb75df5fae85e53568d0a00e1266980e
iAmGroute <pgonis@gmail.com>
2019-06-01 15:24:19 +0300

    Added SlotList

---
+++ NPTpy/Common/SlotList.py
@@ -0,0 +1,105 @@
+# SlotList:
+#   A container based on a fixed length array,
+#   useful for fast allocation, random access and random deallocation.
+#   An ID is returned on allocation, which is unique
+#   until the generation counter overflows (does not happen in Python).
+#   Allocated item slots are not stored in consecutive memory locations,
+#   as the empty slots can be found anywhere in the array.
+
+class Slot:
+
+    # In C, nextFreeIndex and value could be in a union
+    def __init__(self, generation, nextFreeIndex, value):
+        self.gen  = generation
+        self.next = nextFreeIndex
+        self.val  = value
+
+    # Note: python containers call repr() on the items they contain,
+    #       even when str() or format() is called on them
+    def __repr__(self):
+        return '(gen: {0}, next: {1}, val: {2})'.format(self.gen, self.next, self.val)
+
+
+class SlotList:
+
+    def __init__(self, capacityLog2):
+        self.capacityLog2 = capacityLog2
+        self.capacity     = 1 << capacityLog2
+        self.slots        = [Slot(0, i+1, None) for i in range(self.capacity - 1)] + [Slot(0, -1, None)]
+        self.firstFree    = 0
+        self.lastFree     = self.capacity - 1
+
+
+    def __iter__(self):
+        return self.slots.__iter__()
+
+    def __str__(self):
+        return self.slots.__str__()
+
+    def __repr__(self):
+        return self.slots.__repr__()
+
+    def __format__(self):
+        return self.slots.__format__()
+
+
+    def append(self, value):
+        resultID = -1
+        index = self.firstFree
+        if index >= 0:
+            slot = self.slots[index]
+            self.firstFree = slot.next
+            slot.next = -1
+            slot.val  = value
+            resultID  = index + (slot.gen << self.capacityLog2)
+        return resultID
+
+
+    def getIndex(self, ID):
+
+        index = ID & ((1 << self.capacityLog2) - 1)
+        gen   = ID >> self.capacityLog2
+
+        slot = self.slots[index]
+
+        if gen != slot.gen:
+            return -1
+
+        return index
+
+
+    def __getitem__(self, ID):
+        index = self.getIndex(ID)
+        return self.slots[index].val if index >= 0 else None
+
+
+    def __setitem__(self, ID, value):
+
+        if value == None:
+            del self[ID]
+            return
+
+        index = self.getIndex(ID)
+        if index < 0:
+            raise IndexError('ID does not exist or has been superseded')
+
+        self.slots[index] = val
+
+
+    def __delitem__(self, ID):
+
+        index = self.getIndex(ID)
+        if index < 0:
+            return
+
+        slot = self.slots[index]
+        slot.gen += 1
+        slot.val  = None
+
+        if self.firstFree >= 0:
+            self.slots[self.lastFree].next = index
+        else:
+            self.firstFree = index
+
+        self.lastFree = index
+
---
+++ NPTpy/Common/SlotList.test.py
@@ -0,0 +1,95 @@
+
+from SlotList import *
+
+sl = SlotList(2)
+
+assert sl.append('Hello')   == 0
+assert sl.append('World')   == 1
+assert sl.append('Hello 2') == 2
+assert sl.append('World 2') == 3
+assert sl.append('World 3') == -1
+
+assert sl[0] == 'Hello'
+assert sl[1] == 'World'
+assert sl[2] == 'Hello 2'
+assert sl[3] == 'World 2'
+
+del sl[1]
+assert sl[1] == None
+assert sl[5] == None
+
+# Gen should increment
+assert sl.append('Hello 3') == 5
+assert sl[1] == None
+assert sl[5] == 'Hello 3'
+assert sl[9] == None
+
+# Del on old gen should have no effect
+del sl[1]
+assert sl[1] == None
+assert sl[5] == 'Hello 3'
+
+# Assign order should be the same as delete order (oldest first)
+
+del sl[3]
+del sl[0]
+assert sl[3] == None
+assert sl[0] == None
+
+assert sl.append('Hello 4') == 7
+
+del sl[5]
+del sl[2]
+
+assert sl.append('Hello 5') == 4
+assert sl.append('Hello 6') == 9
+assert sl.append('Hello 7') == 6
+
+# Check assignments
+
+for i in range(4):
+    assert sl[i] == None, '@ i == {0}'.format(i)
+
+assert sl[4] == 'Hello 5'
+assert sl[5] == None
+assert sl[6] == 'Hello 7'
+assert sl[7] == 'Hello 4'
+
+assert sl[8] == None
+assert sl[9] == 'Hello 6'
+assert sl[10] == None
+assert sl[11] == None
+
+for i in range(12, 128):
+    assert sl[i] == None, '@ i == {0}'.format(i)
+
+# No more space left
+assert sl.append('World 4') == -1
+
+
+# Delete all slots
+
+del sl[4]
+
+del sl[6]
+del sl[7]
+
+del sl[9]
+
+assert sl.append('Hello 8') == 8
+for i in range(128):
+    assert sl[i] == ('Hello 8' if i == 8 else None), '@ i == {0}'.format(i)
+
+del sl[8]
+
+assert sl.append('Hello 9') == 10
+for i in range(128):
+    assert sl[i] == ('Hello 9' if i == 10 else None), '@ i == {0}'.format(i)
+
+del sl[10]
+
+for i in range(128):
+    assert sl[i] == None, '@ i == {0}'.format(i)
+
+
+print('OK')
