ce828ddd538667c5806c2c1675266ae11ec6d71e
Groute <iAmGroute@outlook.com>
2019-09-01 11:28:21 +0300

    Server: added timeout to listening socket

--- NPTpy/Servers/Server.py
+++ NPTpy/Servers/Server.py
@@ -35,7 +35,7 @@ class Server:
 
         self.relayInfoMessage = relayPort.to_bytes(2, 'little') + bytes(relayAddr, 'utf-8')
 
-        self.con = Connector(log,  Connector.new(socket.SOCK_STREAM, None, port, address))
+        self.con = Connector(log,  Connector.new(socket.SOCK_STREAM, 2, port, address))
         self.con.secureServer(certFilename='server.cer', keyFilename='server.key')
         self.con.listen()
 
