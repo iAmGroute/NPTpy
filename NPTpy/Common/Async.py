03862a86e7857a2a6429f95f0064d514df93df14
Groute <iAmGroute@outlook.com>
2019-10-04 18:58:17 +0300

    Async: all Promises are one-shot, removed Event class

--- NPTpy/Common/Async.py
+++ NPTpy/Common/Async.py
@@ -1,55 +1,28 @@
 import weakref
 
-from .Generic  import noop, identity
+from Generic   import noop
 from .SlotList import SlotList
-
-class Event:
-
-    def __init__(self, oneShot=True, preprocessor=identity, onDelete=noop):
-        pClass = PromiseOneShot if oneShot else Promise
-        self.registrations = pClass(self, 0, preprocessor, isWeak=True)
-        self.onDelete      = onDelete
-        self.disabled      = True
-
-    def __del__(self):
-        self.onDelete(self)
-
-    def cancel(self):
-        return self.registrations.cancel()
-
-    def _cancel(self, pID):
-        self.disabled = True
-
-    def then(self, callback):
-        self.disabled = False
-        return self.registrations.then(callback)
-
-    def fire(self, data=None):
-        self.registrations.fire(data)
-
-    def isOneShot(self):
-        return type(self.registrations) is PromiseOneShot
-
-    def reset(self):
-        if self.isOneShot():
-            self.registrations.hasFired = False
-
 
 class Promise:
 
     def __init__(self, prev, myID, callback, isWeak=False):
-        self.getPrev  = weakref.ref(prev)
+        self.getPrev  = weakref.ref(prev) if prev else noop
         self.myID     = myID
         self.callback = callback
         self.isWeak   = isWeak
         self.next     = SlotList()
+        self.hasFired = False
         self.value    = None
 
-    def then(self, callback, pClass=None):
-        pClass = pClass if pClass else Promise
-        p = pClass(self, -1, callback, isWeak=self.isWeak)
-        pID = self.next.append(p)
+    def reset(self):
+        self.hasFired = False
+
+    def then(self, callback):
+        p      = Promise(self, -1, callback, isWeak=self.isWeak)
+        pID    = self.next.append(p)
         p.myID = pID
+        if self.hasFired:
+            p.fire(self.value)
         return p
 
     def cancel(self):
@@ -63,34 +36,17 @@ class Promise:
             self.cancel()
 
     def fire(self, data):
+        self.hasFired = True
         self.value = self.callback(data)
         for p in self.next:
             p.fire(self.value)
+        self.cancel()
 
 
-class PromiseOneShot(Promise):
-
-    def __init__(self, *args, **kwargs):
-        Promise.__init__(self, *args, **kwargs)
-        self.hasFired = False
-
-    def then(self, callback):
-        p = Promise.then(self, callback, PromiseOneShot)
-        if self.hasFired:
-            p.fire(self.value)
-        return p
-
-    def fire(self, data):
-        if not self.hasFired:
-            self.hasFired = True
-            Promise.fire(self, data)
-            self.cancel()
-
-
-class InstantPromise(PromiseOneShot):
+class InstantPromise(Promise):
 
     def __init__(self, value):
-        PromiseOneShot.__init__(self, self, 0, None)
+        Promise.__init__(self, None, 0, None)
         self.hasFired = True
         self.value    = value
 
