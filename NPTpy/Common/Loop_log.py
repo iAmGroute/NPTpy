43c578dc5fffcb4a82a09b0fa1739508ae9c2e4e
Groute <iAmGroute@outlook.com>
2019-11-26 08:40:49 +0200

    Loop_log: followup

--- NPTpy/Common/Loop_log.py
+++ NPTpy/Common/Loop_log.py
@@ -17,11 +17,11 @@ class LogClass:
     name     = 'Loop'
     etypes   = Etypes
     disabled = [
-        Watching,
-        Resolving,
-        Continuing,
-        Running,
-        Finished,
-        Paused,
+        Etypes.Watching,
+        Etypes.Resolving,
+        Etypes.Continuing,
+        Etypes.Running,
+        Etypes.Finished,
+        Etypes.Paused,
     ]
 
