d73a984b38b6d45f82ba0c93c9c87a664d357c1a
Groute <iAmGroute@outlook.com>
2019-12-11 00:01:56 +0200

    Create Promises.py

---
+++ NPTpy/Common/Promises.py
@@ -0,0 +1,36 @@
+import weakref
+
+from .Generic import nop, toTuple
+from .SlotMap import SlotMap
+from .Async   import Promise
+
+class Promises:
+
+    def __init__(self, timeoutReminder, timeoutFunction=nop):
+        self.items           = SlotMap()
+        self.reminder        = timeoutReminder.new(owner=self, onRun=Promises.handleRemind)
+        self.timeoutFunction = timeoutFunction
+
+    def handleRemind(self):
+        for k, v in self.items.iterKV():
+            promise, stale, meta = v
+            if not stale:
+                self.items[k] = promise, True, meta
+            else:
+                del self.items[k]
+                try:
+                    params = self.timeoutFunction(meta)
+                except AssertionError:
+                    params = None
+                else:
+                    p.fire(toTuple(params))
+
+    def _cancel(self, promiseID):
+        del self.items[promiseID]
+
+    def new(self, meta=None):
+        p         = Promise()
+        p.myID    = self.items.append((p, False, meta))
+        p.getPrev = weakref.ref(self)
+        return p
+
