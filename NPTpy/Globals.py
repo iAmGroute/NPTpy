a6a353e3a51c1193bdf94d3e657f34f4b99c8585
Groute <iAmGroute@outlook.com>
2019-12-11 00:02:59 +0200

    Globals: added timeoutReminder

--- NPTpy/Globals.py
+++ NPTpy/Globals.py
@@ -5,8 +5,9 @@ from Common.Reminder      import Reminder
 from Common.TimedReminder import TimedReminder
 from Common.Selectables   import Selectables
 
-kaReminderRX = TimedReminder(interval=30.0)
-kaReminderTX = TimedReminder(interval=10.0)
+kaReminderRX    = TimedReminder(interval=30.0)
+kaReminderTX    = TimedReminder(interval=10.0)
+timeoutReminder = TimedReminder(interval=50.0)
 
 def runReminders():
     kaReminderRX.run()
