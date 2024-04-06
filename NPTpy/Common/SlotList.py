cc75da6f321a0e4f53855306249e10569f28f270
iAmGroute <iAmGroute@netport.io>
2024-04-06 16:16:02 +0300

    SlotList: make some names non-public

--- NPTpy/Common/SlotList.py
+++ NPTpy/Common/SlotList.py
@@ -12,7 +12,7 @@ from typing import Callable, Generic, Iterable, List, Optional, TypeVar
 T = TypeVar('T')
 
 
-class Slot(Generic[T]):
+class _Slot(Generic[T]):
 
     # In C, <nextFree> and <value> could be in a union
     def __init__(self, myID: int, nextFree: int, value: Optional[T]):
@@ -33,20 +33,20 @@ class SlotList(Generic[T]):
 
     def __init__(self, values: Iterable[T] = [], initial_capacity = 2):
         assert initial_capacity >= 2, initial_capacity # TODO: can we allow == 1?
-        self.count     = 0
-        self.slots     : List[Slot[T]] \
-                       = [Slot(i, i+1, None) for i in range(initial_capacity)]
-        self.slots[-1].nextFree = -1
-        self.firstFree = 0
-        self.lastFree  = initial_capacity - 1
+        self._count     = 0
+        self._slots     : List[_Slot[T]] \
+                        = [_Slot(i, i+1, None) for i in range(initial_capacity)]
+        self._slots[-1].nextFree = -1
+        self._firstFree = 0
+        self._lastFree  = initial_capacity - 1
         for value in values:
             self.append(value)
 
     def __len__(self):
-        return self.count
+        return self._count
 
     def capacity(self):
-        return len(self.slots)
+        return len(self._slots)
 
     def isFull(self):
         return len(self) == self.capacity()
@@ -57,19 +57,19 @@ class SlotList(Generic[T]):
     def __iter__(self):
         yield from (
             slot.val
-            for slot in self.slots
+            for slot in self._slots
             if slot.val is not None
         )
 
     def iterKV(self):
         yield from (
             (slot.myID, slot.val)
-            for slot in self.slots
+            for slot in self._slots
             if slot.val is not None
         )
 
     def listIDs(self):
-        return [s.myID for s in self.slots if s]
+        return [s.myID for s in self._slots if s]
 
     def prettyPrint(self, f: Callable[[T], str]):
         result = [f(val) for val in self]
@@ -79,16 +79,16 @@ class SlotList(Generic[T]):
         return self.prettyPrint(repr)
 
     def __repr__(self):
-        return 'SlotList' + repr(self.slots)
+        return 'SlotList' + repr(self._slots)
 
     def __format__(self, fmt: str):
         return self.prettyPrint(lambda x: format(x, fmt))
 
     def fixFreeIndexes(self):
-        indexes = [i for i in range(self.capacity()) if not self.slots[i]]
+        indexes = [i for i in range(self.capacity()) if not self._slots[i]]
         self.firstFree = indexes[0]
         for i in range(len(indexes) - 1):
-            self.slots[indexes[i]].nextFree = indexes[i + 1]
+            self._slots[indexes[i]].nextFree = indexes[i + 1]
         self.lastFree  = indexes[-1]
 
     def grow(self):
@@ -97,11 +97,11 @@ class SlotList(Generic[T]):
         new_cap = 2 * cap
         mask    = new_cap - 1
         for i in range(cap):
-            slot              = self.slots[i]
-            self.slots.append(slot)
-            newSlot           = Slot(slot.myID + cap, -1, None)
+            slot              = self._slots[i]
+            self._slots.append(slot)
+            newSlot           = _Slot(slot.myID + cap, -1, None)
             index             = newSlot.myID & mask
-            self.slots[index] = newSlot
+            self._slots[index] = newSlot
         assert self.capacity() == new_cap, (self, new_cap)
         self.fixFreeIndexes()
 
@@ -109,61 +109,61 @@ class SlotList(Generic[T]):
         if self.firstFree < 0:
             self.grow()
         index          = self.firstFree
-        slot           = self.slots[index]
+        slot           = self._slots[index]
         slot.val       = value
         self.firstFree = slot.nextFree
-        self.count    += 1
+        self._count    += 1
         return slot.myID
 
-    def getIndex(self, ID: int):
+    def _getIndex(self, ID: int):
         index = ID & (self.capacity() - 1)
-        slot  = self.slots[index]
+        slot  = self._slots[index]
         return index if slot.myID == ID else -1
 
-    def deleteByIndex(self, index: int):
-        slot            = self.slots[index]
+    def _deleteByIndex(self, index: int):
+        slot            = self._slots[index]
         if slot:
-            self.count -= 1
+            self._count -= 1
         slot.myID      += self.capacity()
         slot.nextFree   = -1
         slot.val        = None
         if self.firstFree >= 0:
-            self.slots[self.lastFree].nextFree = index
+            self._slots[self.lastFree].nextFree = index
         else:
             self.firstFree = index
         self.lastFree = index
 
     def deleteAll(self):
-        maxID = max(self.slots, key=lambda s: s.myID).myID
+        maxID = max(self._slots, key=lambda s: s.myID).myID
         maxID = (maxID | 1) + 1
         self.__init__()
-        self.slots[0].myID = maxID
-        self.slots[1].myID = maxID + 1
+        self._slots[0].myID = maxID
+        self._slots[1].myID = maxID + 1
 
     def __getitem__(self, ID: int):
-        index = self.getIndex(ID)
-        return self.slots[index].val if index >= 0 else None
+        index = self._getIndex(ID)
+        return self._slots[index].val if index >= 0 else None
 
     def __setitem__(self, ID: int, value: Optional[T]):
         if value is None:
             del self[ID]
             return
-        index = self.getIndex(ID)
+        index = self._getIndex(ID)
         if index < 0:
             raise IndexError(f'ID {ID} does not exist or has been superseded')
-        self.slots[index].val = value
+        self._slots[index].val = value
 
     def __delitem__(self, ID: int):
-        index = self.getIndex(ID)
+        index = self._getIndex(ID)
         if index < 0:
             return
-        self.deleteByIndex(index)
+        self._deleteByIndex(index)
 
     def pop(self, ID: int):
-        index = self.getIndex(ID)
+        index = self._getIndex(ID)
         if index < 0:
             return None
-        res = self.slots[index].val
-        self.deleteByIndex(index)
+        res = self._slots[index].val
+        self._deleteByIndex(index)
         return res
 
