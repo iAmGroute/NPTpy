628e8a5aaeaf177e18e28c38e730b77d72229a94
iAmGroute <iAmGroute@netport.io>
2024-04-07 10:49:28 +0300

    Container_test: update

--- NPTpy/Common/Container_test.py
+++ NPTpy/Common/Container_test.py
@@ -1,8 +1,13 @@
 
 import random
 
+from typing import Type, Union
 
-def runContainer(Container):
+from .SlotList       import SlotList
+from .SlotListPacked import SlotListPacked
+
+
+def run_container(Container: Union[Type[SlotList], Type[SlotListPacked]]):
 
     sl = Container()
 
@@ -14,96 +19,125 @@ def runContainer(Container):
     assert sl.append('Hello 1') == 1 and len(sl) == 2
     assert sl.append('Hello 2') == 2 and len(sl) == 3
     assert sl.append('Hello 3') == 3 and len(sl) == 4
+    assert sl.append('Hello 4') == 4 and len(sl) == 5
 
     # Accessible (only) by given IDs
     assert sl[0] == 'Hello 0'
     assert sl[1] == 'Hello 1'
     assert sl[2] == 'Hello 2'
     assert sl[3] == 'Hello 3'
-    assert sl[4] == None
+    assert sl[4] == 'Hello 4'
+    assert sl[5] == None
 
     # Deletion
-    del sl[1]
-    assert len(sl) == 3
-    assert sl[1] == None
+    del    sl[1]
+    assert len(sl) == 4
+    assert sl[0] == 'Hello 0'
+    assert sl[1] == None # deleted
+    assert sl[2] == 'Hello 2'
     assert sl[3] == 'Hello 3'
-    assert sl[4] == None
+    assert sl[4] == 'Hello 4'
+    assert sl[5] == None # never allocated
+    assert len(sl) == 4
+
+    # Deletion by setting to None
+    sl[4] = None
+    assert len(sl) == 3
+    assert sl[0] == 'Hello 0'
+    assert sl[1] == None
+    assert sl[2] == 'Hello 2'
+    assert sl[3] == 'Hello 3'
+    assert sl[4] == None # just deleted
+    assert sl[5] == None
+    assert len(sl) == 3
 
     # Iteration
-    items = list(sl)
-    items.sort()
-    assert items == ['Hello 0', 'Hello 2', 'Hello 3']
-    assert sl.listIDs() == [0, 2, 3]
+    assert sorted(sl) == ['Hello 0', 'Hello 2', 'Hello 3']
+    # Key iteration
+    assert sorted(sl.keys()) == [0, 2, 3]
+    # Value iteration
+    assert sorted(sl.values()) == ['Hello 0', 'Hello 2', 'Hello 3']
+    # Key-Value iteration
+    assert sorted(sl.items()) == [(0, 'Hello 0'), (2, 'Hello 2'), (3, 'Hello 3')]
+    # Plain iteration same as value iteration
+    assert list(sl) == list(sl.values())
 
-    # IDs should increment and old ones be invalid
-    newID = sl.append('Hello new')
-    assert newID > 3
+    # Keys should increment and old ones be invalid
+    new_key = sl.append('Hello new')
+    assert new_key > 4
+    assert len(sl) == 4
+    assert sl[0] == 'Hello 0'
     assert sl[1] == None
+    assert sl[2] == 'Hello 2'
     assert sl[3] == 'Hello 3'
-    assert sl[newID] == 'Hello new'
+    assert sl[4] == None
+    assert sl[new_key] == 'Hello new'
 
     # 'del' on old IDs should have no effect
-    del sl[1]
+    del    sl[1]
+    del    sl[4]
+    sl[1] = None
+    sl[4] = None
+    assert len(sl) == 4
+    assert sl[0] == 'Hello 0'
     assert sl[1] == None
+    assert sl[2] == 'Hello 2'
     assert sl[3] == 'Hello 3'
-    assert sl[newID] == 'Hello new'
+    assert sl[4] == None
+    assert sl[new_key] == 'Hello new'
 
     # pop()
     assert sl.pop(1) == None
+    assert len(sl) == 4
     assert sl.pop(3) == 'Hello 3'
-    assert sl[1]     == None
-    assert sl[3]     == None
-    assert sl[newID] == 'Hello new'
-    items = list(sl)
-    items.sort()
-    assert items == ['Hello 0', 'Hello 2', 'Hello new']
-    items = sl.listIDs()
-    items.sort()
-    assert items == [0, 2, newID]
+    assert len(sl) == 3
+    assert sl[0] == 'Hello 0'
+    assert sl[1] == None
+    assert sl[2] == 'Hello 2'
+    assert sl[3] == None
+    assert sl[4] == None
+    assert sl[new_key] == 'Hello new'
 
     del sl
 
     # Initial values as constructor parameter
     ivs = ['World 0', 'World 1', 'World 2', 'World 3']
     sl  = Container(ivs)
-    assert list(sl) == ivs
+    assert sorted(sl) == ivs
 
     del sl
 
-    # Iteration KV
-    original = list(range(777))
+    # Iteration
+    original = list(range(15, 777, 2))
     sl       = Container(original)
-    items    = list(sl.iterKV())
-    assert items == [(i, i) for i in original]
+    assert list(sl.items()) == [(i, original[i]) for i in range(len(original))]
 
-    # Iteration KV allows deletion of the current item
-    original = list(range(555))
+    # Key-Value iteration allows deletion of the current item
+    original = list(range(15, 55, 5))
     sl       = Container(original)
-
-    items = []
-    for k, v in sl.iterKV():
+    items    = []
+    for k, v in sl.items_mutable():
         if k % 3 == 0:
             del sl[k]
         items.append(v)
-
     items.sort()
     assert items == original
 
-    expected = [original[i] for i in range(len(original)) if i % 3 != 0]
-    items    = list(sl)
+    expected = [(i, original[i]) for i in range(len(original)) if i % 3 != 0]
+    items    = list(sl.items())
     items.sort()
     assert items == expected
 
     del sl
 
-    # Iteration KV doesn't crash if the container is changed between iterations
-    original = list(range(5))
+    # Key-Value iteration doesn't crash if the container is changed between iterations
+    original = list(range(5, 25, 2))
     sl       = Container(original)
     i = 0
-    for k, v in sl.iterKV():
+    for k, v in sl.items():
         del sl[2]
         if   i == 1:
-            sl.append('hello')
+            sl.append(1111)
         elif i >= 3:
             del sl[4]
             del sl[5]
@@ -111,10 +145,12 @@ def runContainer(Container):
         i += 1
 
     items = list(sl)
-    items.append(2)
-    items.append(4)
+    items.append(original[2])
+    items.append(original[4])
+    items.append(original[5])
+    items.append(original[6])
     items.sort()
-    assert items == original
+    assert items == original + [1111]
 
     del sl
     # Grow after deletion
@@ -124,35 +160,35 @@ def runContainer(Container):
     sl.append('2')
     sl.append('3')
     del sl[2]
-    newID  = sl.append('new')
+    new_key  = sl.append('new')
     newID2 = sl.append('new2')
     assert sl[0] == '0'
     assert sl[1] == '1'
     assert sl[2] == None
     assert sl[3] == '3'
-    assert sl[newID]  == 'new'
+    assert sl[new_key]  == 'new'
     assert sl[newID2] == 'new2'
 
     del sl
 
-    # Assign order should be the same as delete order (oldest first / LRU)
+    # Assign order should be the same as delete order (oldest first / FIFO)
     sl = Container()
-    sl.append('0')
-    sl.append('1')
-    sl.append('2')
-    sl.append('3')
+    sl.append('t0')
+    sl.append('t1')
+    sl.append('t2')
+    sl.append('t3')
     del sl[3]
     del sl[0]
     assert sl[3] == None
     assert sl[0] == None
     a = sl.append('Hello a')
     b = sl.append('Hello b')
-    # without LRU, we would expect 'b > a' (since 'b' is added later)
-    # with LRU, 'a' should go where '3' previously was, and 'b' where '0' was:
+    # with LIFO, we would expect `b > a` (since `b` is added later)
+    # with FIFO, `a` should go where `t3` previously was, and `b` where `t0` was:
     assert a > b
 
-    # deleteAll()
-    sl.deleteAll()
+    # clear()
+    sl.clear()
     assert len(sl) == 0
     assert sl.append('Test') > a
 
@@ -186,16 +222,11 @@ def runContainer(Container):
     del d
     del sd
 
-    # OK
-    print('OK')
+
+def test_SlotList():
+    run_container(SlotList)
 
 
-def testSlotList():
-    from .SlotList import SlotList
-    runContainer(SlotList)
-
-
-def testSlotMap():
-    from .SlotMap import SlotMap
-    runContainer(SlotMap)
+def test_SlotListPacked():
+    run_container(SlotListPacked)
 
