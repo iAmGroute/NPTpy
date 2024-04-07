73ab6a338ea70d0a4e7d127eadf5b22f60eaa333
iAmGroute <iAmGroute@netport.io>
2024-04-07 10:48:24 +0300

    Generic: add short_str()

--- NPTpy/Common/Generic.py
+++ NPTpy/Common/Generic.py
@@ -1,4 +1,6 @@
 
+from typing import Any
+
 def nop(*args, **kwargs):
     # pylint: disable=unused-argument
     return None
@@ -27,6 +29,13 @@ def runAndRemove(aSet, f):
             dead.add(item)
     aSet -= dead
     return len(dead)
+
+def short_str(x: Any, max_len = 20):
+    assert max_len > 2, (x, max_len)
+    s = str(x)
+    l = len(s)
+    return s if l <= max_len else s[:(max_len+1)//2-1] + '..' + s[l-max_len//2+1:]
+    # NOTE: (-x//2) rounds down, as `-`` is inversion, while (a-x//2) rounds up, as `-` is subtraction
 
 # Like weakref.ref but not weak, intended for consistency
 # when mixing refs and weak refs in countainers.
